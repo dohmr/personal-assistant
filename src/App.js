9de8fe8b4dc66cbb024ba1d969a3f08443058a47
doh <theshawns0n@gmail.com>
2021-01-08 14:39:38 -0500

    adding content for skills focus

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import Particles from "react-particles-js"
-import AboutMe from "./components/AboutMe"
+import Particles from "react-particles-js";
+import AboutMe from "./components/AboutMe";
+import Services from "./components/Services"
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Services />
     </>
   );
 }
